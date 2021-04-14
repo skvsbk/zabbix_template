2ea61ef4eee0b3b86ae45dea25a1ec5c2cbbeed4
skvsbk <skv.sbk@mail.ru>
2021-04-14 10:57:50 +0300

    Add bandwith DVR

--- LTV-RNE/get_cams.py
+++ LTV-RNE/get_cams.py
@@ -1,5 +1,11 @@
 #!/usr/bin/env python3
 
+'''
+
+get_cams.py <IP_address> <discovery|element|status>
+
+'''
+
 import sys
 import time
 import json
@@ -11,11 +17,56 @@ from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.ui import WebDriverWait
 
+def get_html(url):
+    driverOptions = webdriver.ChromeOptions()
+    driverOptions.headless = True
+    driverOptions.binary = "/usr/bin/google-chrome-stable"
+    driverOptions.binary_location = "/usr/bin/google-chrome-stable"
+    driverOptions.add_argument("--no-sandbox")
+    driverOptions.add_argument("--disable-setuid-sandbox")
+    driverOptions.add_argument("--disable-dev-shm-usage")
+    driverOptions.add_argument("start-maximized")
+    driverOptions.add_argument("--disable-extensions")
+    driverOptions.add_argument("disable-infobars")
+    driverOptions.add_argument("disable-popup-blocking")
+    driverOptions.add_argument("--disable-extensions")
+    with webdriver.Chrome(executable_path="/opt/WebDriver/bin/chromedriver", options=driverOptions) as driver:
+        driver.get(url_login)
 
-ip_address = sys.argv[1]
-action = sys.argv[2]
+        time.sleep(4)
+        wait = WebDriverWait(driver, 10)
+        wait.until(EC.alert_is_present())
+        alert = driver.switch_to.alert
+        alert.accept()
 
-actionList = ["discovery", "element"]
+        time.sleep(2)
+        username_input = driver.find_element_by_id("txtUserName")
+        username_input.clear()
+        username_input.send_keys(username)
+
+        time.sleep(2)
+        password_input = driver.find_element_by_id("txtPassword")
+        password_input.clear()
+        password_input.send_keys(password)
+        password_input.send_keys(Keys.ENTER)
+        driver.get(url)
+        time.sleep(5)
+
+        return driver.page_source
+
+def get_items(html, tag, class_):
+    soup = BeautifulSoup(html, "lxml")
+    return soup.find_all(tag, class_=class_)
+
+try:
+    ip_address = sys.argv[1]
+    action = sys.argv[2]
+except:
+    print("Empty arguments")
+    sys.exit()
+
+
+actionList = ["discovery", "element", "status"]
 if action not in actionList:
     print("Arguments error")
     sys.exit()
@@ -25,93 +76,68 @@ password = "Admin272218"
 url_login = 'http://' + ip_address + '/#login'
 url_cams = 'http://' + ip_address + '/#config/channel/list'
 url_stat = 'http://' + ip_address + '/#config/system/channel/status'
+url_bandwith = 'http://' + ip_address + '/#config/net/status'
+url_sysinfo = 'http://' + ip_address + '/#config/system/information'
 
-driverOptions = webdriver.ChromeOptions()
-driverOptions.headless = True
-driverOptions.binary = "/usr/bin/google-chrome-stable"
-driverOptions.binary_location = "/usr/bin/google-chrome-stable"
-driverOptions.add_argument("--no-sandbox")
-driverOptions.add_argument("--disable-setuid-sandbox")
-driverOptions.add_argument("--disable-dev-shm-usage")
-driverOptions.add_argument("start-maximized")
-driverOptions.add_argument("--disable-extensions")
-driverOptions.add_argument("disable-infobars")
-driverOptions.add_argument("disable-popup-blocking")
-driverOptions.add_argument("--disable-extensions")
-
-try:
-    driver = webdriver.Chrome(executable_path="/opt/WebDriver/bin/chromedriver", options=driverOptions)
-    driver.get(url_login)
-
-    time.sleep(4)
-    wait = WebDriverWait(driver, 10)
-    wait.until(EC.alert_is_present())
-    alert = driver.switch_to.alert
-    alert.accept()
-
-    time.sleep(2)
-    username_input = driver.find_element_by_id("txtUserName")
-    username_input.clear()
-    username_input.send_keys(username)
-
-    time.sleep(2)
-    password_input = driver.find_element_by_id("txtPassword")
-    password_input.clear()
-    password_input.send_keys(password)
-    password_input.send_keys(Keys.ENTER)
-
-    if action == "discovery":
-        driver.get(url_cams)
-        time.sleep(4)
-        html = driver.page_source
-    else:
-        driver.get(url_stat)
-        time.sleep(4)
-        html = driver.page_source
-finally:
-    driver.close()
-    driver.quit()
-
-cams_list = []
+elements_list = []
 elements_dict = dict()
 cam_count = 1
 
-soup = BeautifulSoup(html, "lxml")
-cams = soup.find_all("tr", class_="grid-row")
-
 if action == "discovery":
-    for cam in cams:
-        cam_dict = dict()
+    html = get_html(url_cams)
+    items = get_items(html, "tr", "grid-row")
+    for item in items:
+        items_dict = dict()
         try:
-            cam_number = cam.find_all("td", class_="grid-general-text")[1].text
+            cam_number = item.find_all("td", class_="grid-general-text")[1].text
         except:
             cam_number = None
         try:
-            cam_name = cam.find_all("td", class_="grid-general-text")[2].text
+            cam_name = item.find_all("td", class_="grid-general-text")[2].text
         except:
             cam_name = None
         try:
-            cam_ip = cam.find_all("td", class_="grid-general-text")[3].text
+            cam_ip = item.find_all("td", class_="grid-general-text")[3].text
         except:
             cam_ip = None
         try:
-            cam_model = cam.find_all("td", class_="grid-general-text")[7].text
+            cam_model = item.find_all("td", class_="grid-general-text")[7].text
         except:
             cam_model = None
-        cam_dict["{#CAMNUMBER}"] = cam_number
-        cam_dict["{#CAMNAME}"] = cam_name.replace("\xa0", " ").strip()
-        cam_dict["{#CAMIP}"] = cam_ip
-        cam_dict["{#CAMMODEL}"] = cam_model
-        cams_list.append(cam_dict)
-else:
-    for cam in cams:
+        items_dict["{#CAMNUMBER}"] = cam_number
+        items_dict["{#CAMNAME}"] = cam_name.replace("\xa0", " ").strip()
+        items_dict["{#CAMIP}"] = cam_ip
+        items_dict["{#CAMMODEL}"] = cam_model
+        elements_list.append(items_dict)
+
+    print('{"data":' + json.dumps(elements_list, indent=4, ensure_ascii=False) + "}")
+
+elif action == "element":
+    html = get_html(url_stat)
+    items = get_items(html, "tr", "grid-row")
+    for item in items:
         try:
-            cam_status = cam.find_all("td")[3].text
+            cam_status = item.find_all("td")[3].text
         except:
             cam_status = ""
-        elements_dict[cam_count] = 1 if cam_status.replace("\xa0", " ") == "Online" else 0
+        elements_dict[cam_count] = 1 if cam_status.replace("\xa0", " ").strip() == "Online" else 0
         cam_count +=1
 
-result = json.dumps(cams_list, indent=4, ensure_ascii=False)
+    print(json.dumps(elements_dict, indent=4, ensure_ascii=False))
 
-print('{"data":' + result + "}") if action == "discovery" else print(json.dumps(elements_dict, indent=4, ensure_ascii=False))
+else:
+    html = get_html(url_bandwith)
+    items = get_items(html, "td", "grid-general-text")
+    for n in range(0, len(items)-1, 2):
+        key = items[n].get('title').replace("\xa0", " ").strip()
+        val = items[n+1].get('title').replace("\xa0", " ").strip()
+        if key == 'Total Bandwidth':
+            elements_dict['BWINTOTAL'] = val.replace("Mb", " ").strip()
+        if key == 'Real-time Remain Bandwidth' or key == 'Remain Bandwidth':
+            elements_dict['BWINREM'] = val.replace("Mb", " ").strip()
+        if key == 'Total Send Bandwidth':
+            elements_dict['BWOUTTOTAL'] = val.replace("Mb", " ").strip()
+        if key == 'Real-time Remain Send Bandwidth' or key == 'Remain Send Bandwidth':
+            elements_dict['BWOUTREM'] = val.replace("Mb", " ").strip()
+
+    print(json.dumps(elements_dict, indent=4, ensure_ascii=False))
